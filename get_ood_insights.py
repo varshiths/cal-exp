ad95dd25ee5232e58fe72de4e0a36a12d4c9388c
Varshith S <varshith.lfhs@gmail.com>
2018-09-03 23:08:08 +0530

    update ood insights

--- get_ood_insights.py
+++ get_ood_insights.py
@@ -5,223 +5,103 @@ import sys
 import matplotlib
 import matplotlib.pyplot as plt
 
-matplotlib.use('GTK')
-plt.style.use('ggplot')
-
 import numpy as np 
 
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file_main', type=str, required=True,
+parser.add_argument('--file', type=str, required=True,
                     help='The path to the output file for main model')
 parser.add_argument('--hinged', type=bool, default=True,
                     help='Whether model outputs extra logit')
-parser.add_argument('--N', type=int, default=20,
-                    help='Number of bins')
 args = parser.parse_args()
 
 EPSILON=1e-7
 
-N = args.N
 if args.hinged:
     _NCLASSES = 11
 else:
     _NCLASSES = 10
 _OOD_CLASS = 10
 
-def transform_target_line(tline):
-    tline = tline[tline.index('Targets[')+8:]
+def transform_line(tline, skey):
+    tline = tline[tline.index(skey)+len(skey):]
     tline = tline.replace('][', ' ')
     tline = tline.replace(']', '')
     tline = tline.replace('[', '')
     tline = tline.replace('\n', '')
     tline = tline.split(' ')
 
-    tline = [int(x) for x in tline]
+    tline = [float(x) for x in tline]
     tline = np.array(tline)
 
     return tline
 
-def transform_probs_line(pline):
-    pline = pline[pline.index('Probs[')+6:]
-    pline = pline.replace('][', ' ')
-    pline = pline.replace(']', '')
-    pline = pline.replace('[', '')
-    pline = pline.replace('\n', '')
-    pline = pline.split(' ')
-    pline = [float(x) for x in pline]
-    
-    pline = np.array(pline)
-    pline = np.reshape(pline, [-1, _NCLASSES])
+tensors_to_get = ["Targets[", "Probs[", "Logits["]
 
-    return pline
+def get_tensors_from_file(filename):
 
-def transform_logits_line(lline):
-    lline = lline[lline.index('Logits[')+7:]
-    lline = lline.replace('][', ' ')
-    lline = lline.replace(']', '')
-    lline = lline.replace('[', '')
-    lline = lline.replace('\n', '')
-    lline = lline.split(' ')
-    lline = [float(x) for x in lline]
-    
-    lline = np.array(lline)
-    lline = np.reshape(lline, [-1, _NCLASSES])
-
-    return lline
-
-def transform_features_line(lline):
-    lline = lline[lline.index('Features[')+9:]
-    lline = lline.replace('][', ' ')
-    lline = lline.replace(']', '')
-    lline = lline.replace('[', '')
-    lline = lline.replace('\n', '')
-    lline = lline.split(' ')
-    lline = [float(x) for x in lline]
-    
-    lline = np.array(lline)
-    lline = np.reshape(lline, [-1, 32, 32])
-
-    return lline
-
-
-def get_targets_and_probs_from_file(filename):
-    targets = []
-    probs = []
-    logits = []
-    features = []
+    tsrsd = {}
+    for tsr in tensors_to_get:
+        tsrsd[tsr] = []
 
     with open(filename, 'r') as f:
         line = f.readline()
         while line:
-            if 'Targets' in line:
-                targets.append(transform_target_line(line))
-            if 'Probs' in line:
-                probs.append(transform_probs_line(line))
-            if 'Logits' in line:
-                logits.append(transform_logits_line(line))
-            # if 'Features' in line:
-            #     features.append(transform_features_line(line))
+            for tsr in tensors_to_get:
+                if tsr in line:
+                    tsrsd[tsr].append(transform_line(line, tsr))
             line = f.readline()
 
-    targets = np.concatenate(targets, axis=0)
-    probs = np.concatenate(probs, axis=0)
-    logits = np.concatenate(logits, axis=0)
-    # features = np.concatenate(features, axis=0)
+    tsrsd = { x: np.concatenate(y, axis=0) for x, y in tsrsd.items() }
+
+    targets = tsrsd[tensors_to_get[0]]
+    probs = tsrsd[tensors_to_get[1]]
+    logits = tsrsd[tensors_to_get[2]]
+
+    # perform the required reshapes
+    targets = targets
+    probs = np.reshape(probs, [-1, _NCLASSES])
+    logits = np.reshape(logits, [-1, _NCLASSES])
 
     assert targets.shape[0] == probs.shape[0], "corrupt: %s" % filename
-    # return targets, probs, logits, features
-    return targets, probs, logits, None
+    return targets, probs, logits
 
 def main():
 
-    targets, probs_main, logits_main, _ = get_targets_and_probs_from_file(args.file_main)
+    targets, probs, logits = get_tensors_from_file(args.file)
 
-    pred_main = np.argmax(probs_main, axis=1)
-    mprobs_main = np.max(probs_main, axis=1)
+    preds = np.argmax(probs, axis=1)
+    mprobs = np.max(probs, axis=1)
+    mlogits = np.max(logits, axis=1)
 
-    mlogits_main = np.max(logits_main, axis=1)
+    omask = (targets == _OOD_CLASS).astype(np.float32)
+    cmask = (preds == targets).astype(np.float32)
 
-    if "only" in args.file_main:
-        ood_mask = targets == _OOD_CLASS
-        corr_mask = pred_main == targets
+    accuracy = np.sum(cmask * (1-omask)) / np.sum(1-omask)
+    detection = np.sum(cmask * omask) / np.sum(omask)
+
+    pcmask = (preds == _OOD_CLASS).astype(np.float32)
+    misdetection = np.sum(pcmask * (1-cmask)) / np.sum(pcmask)
+
+    print("Accuracy: ", accuracy if np.sum(1-omask) != 0 else "No samples")
+    print("Detection: ", detection if np.sum(omask) != 0 else "No samples")
+    print("Misdetection: ", misdetection if np.sum(pcmask) != 0 else "No samples")
+
+    distr_logits(logits, 1-omask, "IND")
+    distr_logits(logits, omask, "OOD")
+
+def distr_logits(logits, mask, name):
+
+    slgts = logits[np.where(mask)]
+    if np.sum(mask) != 0:
+        _min = np.mean(np.min(slgts, axis=1), axis=0)
+        _max = np.mean(np.max(slgts, axis=1), axis=0)
+        _mean = np.mean(np.mean(slgts, axis=1), axis=0)
+        _std = np.mean(np.std(slgts, axis=1), axis=0)
+        print("%s: Min, Max, Mean, Std : %.2f, %.2f, %.2f, %.2f " % (name, _min, _max, _mean, _std))
     else:
-        ood_mask = np.ones(mlogits_main.shape)
-        corr_mask = np.zeros(mlogits_main.shape)
-
-    fmask = ood_mask.astype(np.float32)
-    dmask = 1 - fmask
-
-    accuracy = np.sum(corr_mask.astype(np.float32) * dmask) / np.sum(dmask)
-    detection = np.sum(corr_mask.astype(np.float32) * fmask) / np.sum(fmask)
-
-    print("Accuracy: ", accuracy)
-    print("Detection: ", detection)
-
-    ood_logits = logits_main[np.where(fmask)]
-    id_logits = logits_main[np.where(dmask)]
-    if np.sum(fmask) != 0:
-
-        # import pdb; pdb.set_trace()
-
-        _min = np.mean(np.min(ood_logits, axis=1), axis=0)
-        _max = np.mean(np.max(ood_logits, axis=1), axis=0)
-        _mean = np.mean(np.mean(ood_logits, axis=1), axis=0)
-        _std = np.mean(np.std(ood_logits, axis=1), axis=0)
-
-        print("OOD: Min/Max/Mean/Std : %.2f/%.2f/%.2f/%.2f " % (_min, _max, _mean, _std))
-
-    def get_acc_bucket(probs, preds, tgts, confint):
-        # mask = (confint[0] <= probs and probs < confint[1]).astype(int)
-        mask = np.logical_and(confint[0] <= probs, probs < confint[1]).astype(int)
-        # mask = np.logical_and(confint[0] <= probs, probs < confint[1], preds != _OOD_CLASS).astype(int)
-        if np.sum(mask) == 0:
-            accuracy = None
-        else:
-            accuracy = np.sum((tgts == preds).astype(float)*mask) / np.sum(mask)
-        return accuracy, np.sum(mask)
-
-    def get_acc(prob_list, tgt_list, pred_list):
-        avg_accuracy = (np.array(prob_list) >= 0.99)*(np.array(tgt_list) == np.array(pred_list))
-        return np.sum(avg_accuracy)
-
-    def get_intervals(N):
-        int0 = np.arange(N)
-        int1 = int0+1
-        ints = np.stack([int0, int1], axis=1)/N
-        return ints, int0
-
-    ints, indices = get_intervals(args.N)
-    indices = indices/N
-    accur = np.zeros(args.N)
-    freq_base = np.zeros(args.N)
-    freq_main = np.zeros(args.N)
-    plt.figure()
-    plt.xlim(-0.02, 1.02)
-    plt.ylim(-0.02, 1.02)
-
-    # baseline, indices=indices
-    plt.plot(indices, indices, label="reference")
-    
-    # for i in range(ints.shape[0]):
-    #     accur[i], freq_base[i] = get_acc_bucket(mprobs_base, pred_base, targets, ints[i])
-    # plt.plot(indices, freq, label="baseline")
-
-    for i in range(ints.shape[0]):
-        accur[i], freq_main[i] = get_acc_bucket(mprobs_main, pred_main, targets, ints[i])
-    plt.plot(indices, freq_main / np.sum(freq_main), label="frequency")
-
-    plt.plot(indices, accur, label="accuracy")
-
-    plt.legend(loc="upper left")
-    plt.savefig("dump.png")
-    # plt.show()
-
-    # # plot histogram of confidences of outputs
-    # fig = plt.figure()
-
-    # # fig.add_subplot(2, 1, 1)
-    # plt.plot(indices, freq_base / np.sum(freq_base), label="baseline")
-    # # fig.add_subplot(2, 1, 2)
-    # plt.plot(indices, freq_main / np.sum(freq_main), label="main")
-    # plt.legend(loc="upper left")
-
-    # plt.savefig("hist_" + args.file_out)
-
-    # # import pdb; pdb.set_trace()
-
-    # # nsamples = (2, 5)
-    # # indices = np.random.randint(low=0, high=logits.shape[0], size=10)
-    # # features_v = features[indices]
-    # # logits_v = logits[indices]
-
-    # # fig=plt.figure(1)
-    # # for i in range(1, nsamples[0]*nsamples[1]+1):
-    # #     img = images_v[i-1,:,:,:]
-    # #     fig.add_subplot(nsamples[0], nsamples[1], i).title.set_text(labels[i])
-    # #     plt.imshow(img)
-    # # # plt.show()
+        print("%s: No Samples" % (name))
 
 if __name__ == '__main__':
     main()
